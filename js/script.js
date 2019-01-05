f233cd352c9dae95ba653b93ee596ab0439b6044
Dawid Kowalski <dawid.k.kowalski@gmail.com>
2019-01-05 20:15:29 +0100

    Server - show response - main page

--- js/script.js
+++ js/script.js
@@ -1,9 +1,16 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer();
 
+let indexHTML = "...";
+
+fs.readFile("../index.html", function(err, response){
+	indexHTML = response;
+})
+
 server.on("request", function(request, response){
-	response.write("works");
+	response.write(indexHTML);
 	response.end();
 });
 
