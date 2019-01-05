854027ee0e8d6de5de35f7c449036a48995b5d68
Dawid Kowalski <dawid.k.kowalski@gmail.com>
2019-01-05 20:33:26 +0100

    Server - show response - error page

--- js/script.js
+++ js/script.js
@@ -9,9 +9,20 @@ fs.readFile("../index.html", function(err, response){
 	indexHTML = response;
 })
 
+let errImage = "...";
+
+fs.readFile("../images/blad.jpg", function(err, response){
+	errImage = response;
+})
+
 server.on("request", function(request, response){
-	response.write(indexHTML);
-	response.end();
+	if(request.method === "GET" && request.url === "/"){
+		response.write(indexHTML);
+		response.end();
+	} else {
+		response.write(errImage);
+		response.end();
+	}
 });
 
 server.listen(9010);
